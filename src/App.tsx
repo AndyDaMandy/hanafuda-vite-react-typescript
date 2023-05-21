5aa4564060ec13059a733e178d3e91fe6d6d0df5
AndyDaMandy <maldonado.andy.m@gmail.com>
2023-05-21 13:09:43 -0400

    set up card JSON

--- src/App.tsx
+++ src/App.tsx
@@ -15,21 +15,22 @@ function App() {
         <h1 className="text-3xl font-bold underline">
             Hello world!
         </h1>
+        <Game />
     </>
   )
 }
 
 function Board() {
-    //board will consist of an array with two rows of 4.
+    //board will consist of an array with two rows of 5
     //0 means empty, so the board starts empty.
     const BOARD = [
-        [0, 0, 0, 0],
-        [0, 0, 0, 0]
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0]
     ];
     return (
         <div id="board">
             <div id="row-1">
-            <CardSlot value={BOARD[0]}></CardSlot>
+                <CardSlot value={BOARD[0]}/>
             </div>
             <div id="row-2">
 
@@ -37,10 +38,10 @@ function Board() {
         </div>
     )
 }
-function CardSlot({props}){
+function CardSlot({value}){
     return (
         <div>
-            {props}
+            {value}
         </div>
     )
 }
@@ -50,7 +51,63 @@ function Hand() {
 }
 
 function Card() {
-
+    //this is a JSON list of the cards.
+    const CARDS = [
+        {
+            "January": {
+                "cards": [
+                    {
+                        "dreg1": {
+                            "points": 1,
+                            "yaku": 1
+                        }
+                    },
+                    {
+                        "dreg2": {
+                            "points": 1
+                        }
+                    },
+                    {
+                        "dreg2": {
+                            "points": 1
+                        }
+                    },
+                    {
+                        "dreg2": {
+                            "points": 1
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            "February": {
+                "cards": [
+                    {
+                        "dreg1": {
+                            "points": 1,
+                            "yaku": 1
+                        }
+                    },
+                    {
+                        "dreg2": {
+                            "points": 1
+                        }
+                    },
+                    {
+                        "dreg2": {
+                            "points": 1
+                        }
+                    },
+                    {
+                        "dreg2": {
+                            "points": 1
+                        }
+                    }
+                ]
+            }
+        }
+        ]
     return (
         <div>
 
@@ -67,7 +124,31 @@ function Rules(){
 }
 
 function Game() {
+    const [playerScore, setPlayerScore] = useState(0);
+    const [aiScore, setAiScore] = useState(0);
+    const [round, setRound] = useState(1);
 
+    function scoring (): boolean {
+        if (playerScore > aiScore){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    return (
+        <div>
+            <div>
+                Round: {round}
+            </div>
+            <div>
+                Player Score: {playerScore}
+            </div>
+            <div>
+                AI Score: {aiScore}
+            </div>
+        <Board />
+        </div>
+    )
 }
 
 export default App
